aa832cab517c90f8908f019de46db13a26baebcb
Ills-Tms <119455067+Ills-Tms@users.noreply.github.com>
2023-09-05 09:38:36 +0200

    commit correction

---
+++ script.js
@@ -0,0 +1,10 @@
+const h1element = document.querySelector('h1')
+const h2element = document.querySelector('h2')
+
+h1element.addEventListener('click', function clickkezelo() {
+  alert('aaaaaaaaaaaaaa')
+})
+
+h2element.addEventListener('click', function clickkezelo() {
+  alert('aaaaaaaaaaaaaa')
+})
