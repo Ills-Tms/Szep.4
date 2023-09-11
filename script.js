de7ed0e129f876a210696a9536d30dc84c3d5cf2
tamas <you@example.com>
2023-09-11 08:36:47 +0200

    aaaaa

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const h1element = document.querySelector('h1')
 const h2element = document.querySelector('h2')
-
-h1element.addEventListener('click', function clickkezelo() {
-  alert('aaaaaaaaaaaaaa')
+const h1 = document.querySelector('#hello')
+const container = document.querySelector('#container')
+h1element.addEventListener('click', () => {
+  let card = document.createElement('div')
+  card.innerText = Math.floor(Math.random() * 6) + 1
+  container.append(card)
 })
 
 h2element.addEventListener('click', function clickkezelo() {
